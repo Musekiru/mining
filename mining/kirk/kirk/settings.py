3e5dfb1bc86e535244e8af7bec10b7c829093f3b
jgbrika <jgbrika@gmail.com>
2026-01-16 16:54:51 +0100

    req

--- mining/kirk/kirk/settings.py
+++ mining/kirk/kirk/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6+urr*st34h@fs)u8-w22-f6^fd_eusv0c%68yt2ea8kq%t_)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "mining.onrender.com"]
 
